89dd4b8f6625513096dbc25a5abb6d77e329405d
Bench Vue <benchvue101@gmail.com>
2026-08-27 19:06:55 -0400

    Externalize keys to site-keys.js (never reset again)

---
+++ site-keys.js
@@ -0,0 +1,4 @@
+window.HCTP_KEYS = {
+  apiKey: "AIzaSyDzahk0Yhz3faRfMhDS2vQzQ2oADYXhPRg",
+  stats:  "https://script.google.com/macros/s/AKfycbySEQz0u_0F777ouQKlomTV_XveEz6NIPU6i1kO8i5OgKrURyIaWiK-AbLpx_xT6aA5NQ/exec"
+};
